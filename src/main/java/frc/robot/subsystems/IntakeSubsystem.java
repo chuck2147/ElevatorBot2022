de00cacfac228fad57afd611e1249161bd975e91
Asher Nieffenegger <NieffAsh000@students.wvsd.org>
2022-10-24 14:58:19 -0700

    made intake functions with Asher

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -1,10 +1,12 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
@@ -27,8 +29,28 @@ public IntakeSubsystem() {
     rightIntakeMotor.setNeutralMode(NeutralMode.Coast);
     rotateIntakeMotor.setNeutralMode(NeutralMode.Brake);
 
-
-
 }
+
+public void intakeForwardPiston() {
+    rightPiston.set(Value.kForward);
+    leftPiston.set(Value.kForward);
+}
+
+public void intakeReversePiston() {
+    rightPiston.set(Value.kReverse);
+    leftPiston.set(Value.kReverse);
+}
+
+public void intakeMotorForward(){
+
+    rightIntakeMotor.set(ControlMode.PercentOutput, .5);
+    leftIntakeMotor.set(ControlMode.PercentOutput, .5);
+}
+public void intakeMotorReverse(){
+
+    rightIntakeMotor.set(ControlMode.PercentOutput, -.5);
+    leftIntakeMotor.set(ControlMode.PercentOutput, -.5);
+}
+
 
 }
